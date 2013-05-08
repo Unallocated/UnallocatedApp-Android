eda186e843135af27da6dff6120deae2f2e23214
Eric Gragsone <eric.gragsone@erisresearch.org>
2013-05-08 03:34:42 +0000

    Added Android Project files, moved src into src subdirectory.

---
+++ src/org/unallocatedspace/uasapp/InfoPage.java
@@ -0,0 +1,15 @@
+package org.unallocatedspace.uasapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InfoPage extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
