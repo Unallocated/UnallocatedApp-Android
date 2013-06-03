16f5fd6f05bf4a723f9db3d59e394fbfaf5db032
Eric Gragsone <eric.gragsone@erisresearch.org>
2013-06-03 04:47:59 +0000

    Created new Class Data to hold, cache, and keep track of data.

---
+++ src/org/unallocatedspace/uasapp/Data.java
@@ -0,0 +1,97 @@
+import java.lang.String;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+/**
+ * Contains information gathered from the UAS website.
+ */
+public class Data {
+    protected long lastUpdate;
+    protected String message;
+    protected String key;
+
+/**
+ * Constructor.
+ *
+ * If cache doesn't have an entry, do not make one. Wait until setMessage()
+ * has been called.
+ */
+    public Data(String text) {
+        this.key=text;
+
+        /* check if cache already has entry */
+        if(false) {
+        } else {
+            this.lastUpdate=0;
+            this.message="";
+        }
+    }
+
+    public String getLastUpdate() {
+        /** take lastUpdate and convert to string in local TZ */
+        Date now = new Date(this.lastUpdate);
+        SimpleDateFormat date = new SimpleDateFormat();
+        return date.format(now);
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public boolean isStale() {
+        return this.isStale((long) (15*DataTime.MINUTE));
+    }
+
+    public boolean isStale(long duration) {
+        DataTime clock = new DataTime();
+
+        return clock.isExhausted(this.lastUpdate, duration);
+    }
+
+    public void setMessage() {
+        this.setMessage("");
+    }
+
+    public void setMessage(String message) {
+        Calendar now = (Calendar) new GregorianCalendar();
+
+        this.setMessage(message, now.getTimeInMillis());
+    }
+
+    public void setMessage(String message, long timestamp) {
+        Calendar now = (Calendar) new GregorianCalendar();
+
+        if(timestamp > now.getTimeInMillis()) {
+            /* throw exception */
+        } else {
+            /* save to cache here */
+            this.message = message;
+            this.lastUpdate = timestamp;
+        }
+    }
+}
+
+class DataTime {
+    final static int SECOND = 1000;
+    final static long MINUTE = 60*DataTime.SECOND;
+
+    boolean isExhausted(long timestamp, long duration) {
+        Calendar now = (Calendar) new GregorianCalendar();
+
+        return this.isExhausted(timestamp, now.getTimeInMillis(), duration);
+    }
+
+    boolean isExhausted(long startTime, long endTime, long duration) {
+        boolean exhausted = false; 
+
+        if(endTime < startTime) {
+            /* throw exception */
+        } else if((endTime-startTime) > duration) {
+            exhausted=true;
+        }
+
+        return exhausted;
+    }
+}
