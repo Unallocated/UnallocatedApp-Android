8035138ef25109398a0b73f39d9d057fd2c7ba6d
Eric Gragsone <eric.gragsone@erisresearch.org>
2013-06-07 03:03:24 +0000

    Updated documentation of the Data Class

--- src/org/unallocatedspace/uasapp/Data.java
+++ src/org/unallocatedspace/uasapp/Data.java
@@ -9,7 +9,14 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 
 /**
- * Contains information gathered from the UAS website.
+ * Creates time-sensitive data banks.
+ *
+ * Provides caching and couples data with a timestamp metadata. Timestamp is 
+ * useful to determine if data has grown too old and needs to be updated.
+ * <p>
+ * TODO: Overload the Data constructor with one that only requires a text
+ * argument. This constructor takes the from the current activity or
+ * application.
  */
 public class Data {
     private SharedPreferences cache;
@@ -19,10 +26,10 @@ public class Data {
     private static final String TIMESTAMP="Timestamp";
 
 /**
- * Constructor.
+ * Opens a shared preference to cache recent information.
  *
- * If cache doesn't have an entry, do not make one. Wait until setMessage()
- * has been called.
+ * NOTE: If cache doesn't have an entry, do not make one. Let setMessage()
+ * initate the cache.
  */
     public Data(Activity activity, String text) {
         this.key = text;
@@ -30,20 +37,32 @@ public class Data {
         this.editor = cache.edit();
     }
 
+/**
+ * Returns a String with a Formated timestamp of data. 
+ */
     public String getLastUpdate() {
         Date now = new Date(this.cache.getLong(this.TIMESTAMP, 0));
         SimpleDateFormat date = new SimpleDateFormat();
         return date.format(now);
     }
 
+/**
+ * Returns the data stored in this bank or "" if the bank is uninitiated.
+ */
     public String getMessage() {
         return this.cache.getString(this.MESSAGE, "");
     }
 
+/**
+ * Returns true if data stored is older than 15 minutes.
+ */
     public boolean isStale() {
         return this.isStale((long) (15*DataTime.MINUTE));
     }
 
+/**
+ * Returns true if data is older then duration.
+ */
     public boolean isStale(long duration) {
         DataTime clock = new DataTime();
         long timestamp = this.cache.getLong(this.TIMESTAMP, 0);
@@ -51,16 +70,30 @@ public class Data {
         return clock.isExhausted(timestamp, duration);
     }
 
+/**
+ * Clears out current data and reset timestamp to the present.
+ */
     public void setMessage() {
         this.setMessage("");
     }
 
+/**
+ * Sets data with a custom message and updates current timestamp.
+ */
     public void setMessage(String message) {
         Calendar now = (Calendar) new GregorianCalendar();
 
         this.setMessage(message, now.getTimeInMillis());
     }
 
+/**
+ * Sets data with a custom message and an arbitrary timestamp.
+ *
+ * This method throws exception if timestamp is in the future.
+ *
+ * TODO: Throw exception. Perhaps call it a timetravler exception, or the
+ * Doctor exception.
+ */
     public void setMessage(String message, long timestamp) {
         Calendar now = (Calendar) new GregorianCalendar();
 
@@ -74,16 +107,25 @@ public class Data {
     }
 }
 
+/**
+ * Package class to support Data Class.
+ */
 class DataTime {
     final static int SECOND = 1000;
     final static long MINUTE = 60*DataTime.SECOND;
 
+/**
+ * Returns true if timestamp is older than duration.
+ */
     boolean isExhausted(long timestamp, long duration) {
         Calendar now = (Calendar) new GregorianCalendar();
 
         return this.isExhausted(timestamp, now.getTimeInMillis(), duration);
     }
 
+/**
+ * Returns true if startTime is older than endTime by duration seconds.
+ */
     boolean isExhausted(long startTime, long endTime, long duration) {
         boolean exhausted = false; 
 
