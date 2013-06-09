7d91fc8c8b086ef810f814ce02a28012372ce787
Usako <usagi629@gmail.com>
2013-06-09 03:45:06 -0400

    Added UI resources and updated InfoPage Activity to inflate the UI.

--- src/org/unallocatedspace/uasapp/InfoPage.java
+++ src/org/unallocatedspace/uasapp/InfoPage.java
@@ -2,13 +2,20 @@ package org.unallocatedspace.uasapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class InfoPage extends Activity
-{
-    /** Called when the activity is first created. */
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class InfoPage extends Activity {
+	
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	public void launchWallPhoto(View imageBtn) {
+		Log.d("UASApp", "Clicked on the wall image.");
+//		startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("org.unallocatedspace.uasapp/drawable/thewall")));
+	}
 }
